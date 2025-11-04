d9ba054b65056163044c932a247eb74d03910abf
Lethu-hub <124559605+Lethu-hub@users.noreply.github.com>
2025-11-04 12:44:49 +0200

    Update MFC_App.py

--- MFC_App.py
+++ MFC_App.py
@@ -442,8 +442,8 @@ import streamlit_authenticator as stauth
 # -----------------------------
 # Supabase client setup
 # -----------------------------
-url = st.secrets["postgresql://postgres:[M@t3b3l32025]@db.nghahpnwtgqdfokrljhb.supabase.co:5432/postgres"]
-key = st.secrets["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5naGFocG53dGdxZGZva3JsamhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2OTAxODIsImV4cCI6MjA3NzI2NjE4Mn0.35qPtuRd5_BqBZlBFHI6J7f0naJCgNYf5TmalBIN1FE"]
+url = ["postgresql://postgres:[M@t3b3l32025]@db.nghahpnwtgqdfokrljhb.supabase.co:5432/postgres"]
+key = ["eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5naGFocG53dGdxZGZva3JsamhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE2OTAxODIsImV4cCI6MjA3NzI2NjE4Mn0.35qPtuRd5_BqBZlBFHI6J7f0naJCgNYf5TmalBIN1FE"]
 supabase: Client = create_client(url, key)
 
 # -----------------------------
